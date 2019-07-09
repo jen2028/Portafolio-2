e6e9c06d3c316e9870ef289d3e369d9bfd262c98
Jennifer R.  Portilla <49462436+jen2028@users.noreply.github.com>
2019-07-08 21:46:53 -0400

    Add files via upload
    Arbolb

--- main.c
+++ main.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "arbol.h"
+#include<stdlib.h>
+#include<math.h>
+#include "arbb.h"
 
 int main(){
 	
-	char nombre_archivo[50];
-	char caracter;
-	Arbol *un_nodo;
-	un_nodo=NULL;
-	un_nodo=Crea_Arbol(un_nodo,nombre_archivo,caracter);
-	Recorre_Arbol(un_nodo);
+	Arbol *unArbol;
+	int *arreglo;
 	
+	
+	int elem;
+	int h;
+	int posicion;
+	int tam_max;
+	unArbol=NULL;
+	arreglo=NULL;
+	
+    unArbol=Insertar(unArbol,5);
+    unArbol=Insertar(unArbol,3); 
+	unArbol=Insertar(unArbol,2);  
+	unArbol=Insertar(unArbol,1); 
+	
+	
+	MostrarArbol(unArbol);
+	
+	h=altura(unArbol);
+	printf("LA ALTURA ES %d \n",h);
+	tam_max=TamanoMaximoArreglo(unArbol);
+	printf("LA CANTIDAD MAXIMA DE HIJOS ES %d \n",tam_max);
+	
+	arreglo=calloc(tam_max,sizeof(int));
+	
+	
+	printf("El arbol en arreglo es:");
+	Crea_arbol_en_arreglo(unArbol,arreglo,posicion);
+	qsort(arreglo,tam_max,sizeof(int),comparar);
+	
+	
+	MostrarArreglo(arreglo,posicion,tam_max);
+
 }
 
